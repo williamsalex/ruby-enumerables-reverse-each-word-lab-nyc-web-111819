a66fe75badbe0196f14ea803790d367495ffe5ca
Alexander Williams <williams_alex@protonmail.com>
2019-10-10 00:22:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   new_array = string.split(" ")
   for x in new_array
-    new_array[] = x.reverse
+    new_array[new_array.index(x)] = x.reverse
   end
   return new_array
 end
